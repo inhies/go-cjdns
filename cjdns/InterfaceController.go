aa4e55404c2ee755d5e196cb89c213dc17cf5aa6
inhies <inhies@gmail.com>
2013-09-06 08:49:03 -1000

    Use better types for peer state data

--- cjdns/InterfaceController.go
+++ cjdns/InterfaceController.go
@@ -1,17 +1,55 @@
 package cjdns
 
+import (
+	"strings"
+	"time"
+)
+
+type PeerState int
+
+// Peer state values
+const (
+	Unauthenticated = iota
+	Handshake
+	Established
+	Unresponsive
+)
+
+var (
+	peerStateStrings = [4]string{
+		"UNAUTHENTICATED",
+		"HANDSHAKE",
+		"ESTABLISHED",
+		"UNRESPONSIVE",
+	}
+)
+
+func (s PeerState) String() string {
+	if s.Int() < 0 || s.Int() > len(peerStateStrings)-1 {
+		return "INVALID"
+	}
+	return peerStateStrings[s]
+}
+
+func (s PeerState) Int() int {
+	return int(s)
+}
+
+// Peer statistics
 type PeerStats struct {
-	Last        int64
-	SwitchLabel string
-	BytesOut    int64
-	PublicKey   string
-	State       string
-	IsIncoming  bool
-	BytesIn     int64
+	PublicKey   string    // Public key of peer
+	SwitchLabel string    // Internal switch label for reaching the peer
+	IsIncoming  bool      // Is the peer connected to us, or us to them
+	BytesOut    int64     // Total number of bytes sent
+	BytesIn     int64     // Total number of bytes received
+	State       PeerState // Peer connection state
+	Last        time.Time // Last time a packet was received from the peer
 }
 
 // Returns stats on currently connected peers
-func InterfaceController_peerStats(user *Conn, page int) (response []PeerStats, err error) {
+func InterfaceController_peerStats(user *Conn, page int) (
+	response []PeerStats, err error) {
+
 	args := make(map[string]interface{})
 
 	args["page"] = page
@@ -26,16 +64,30 @@ func InterfaceController_peerStats(user *Conn, page int) (response []PeerStats, 
 	response = make([]PeerStats, 0)
 	for _, peer := range data["peers"].([]interface{}) {
 		info := peer.(map[string]interface{})
+
+		// Convert the int to a bool
 		var incoming bool
 		if info["isIncoming"].(int64) > 0 {
 			incoming = true
 		}
+
+		// Convert connection state to an int
+		var state PeerState
+		tu := strings.ToUpper(info["state"].(string))
+		for i, name := range peerStateStrings {
+			if name == tu {
+				state = PeerState(i)
+			}
+		}
+		// Convert the last packet received timestamp to a time.Time
+		last := time.Unix(info["last"].(int64), 0)
+
 		peer := PeerStats{
-			Last:        info["last"].(int64),
+			Last:        last,
 			BytesIn:     info["bytesIn"].(int64),
 			BytesOut:    info["bytesOut"].(int64),
 			IsIncoming:  incoming,
-			State:       info["state"].(string),
+			State:       state,
 			PublicKey:   info["publicKey"].(string),
 			SwitchLabel: info["switchLabel"].(string),
 		}
