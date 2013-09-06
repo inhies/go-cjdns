8f58b4c84bd9160bc3bf11b07f00278d10423784
inhies <inhies@gmail.com>
2013-09-05 17:15:47 -1000

    Return peer stat struct instead of map

--- cjdns/InterfaceController.go
+++ cjdns/InterfaceController.go
@@ -1,17 +1,47 @@
 package cjdns
 
+type PeerStats struct {
+	Last        int64
+	SwitchLabel string
+	BytesOut    int64
+	PublicKey   string
+	State       string
+	IsIncoming  bool
+	BytesIn     int64
+}
+
 // Returns stats on currently connected peers
-func InterfaceController_peerStats(user *Conn, page int) (response map[string]interface{}, err error) {
+func InterfaceController_peerStats(user *Conn, page int) (response []PeerStats, err error) {
 	args := make(map[string]interface{})
 
 	args["page"] = page
 
-	response, err = SendCmd(user, "InterfaceController_peerStats", args)
+	data, err := SendCmd(user, "InterfaceController_peerStats", args)
 	if err != nil {
 		return
+	}
+
+	// Convert the map to a slice of structs.
+	// This should be fixed so ALL functions return structs... eventually...
+	response = make([]PeerStats, 0)
+	for _, peer := range data["peers"].([]interface{}) {
+		info := peer.(map[string]interface{})
+		var incoming bool
+		if info["isIncoming"].(int64) > 0 {
+			incoming = true
+		}
+		peer := PeerStats{
+			Last:        info["last"].(int64),
+			BytesIn:     info["bytesIn"].(int64),
+			BytesOut:    info["bytesOut"].(int64),
+			IsIncoming:  incoming,
+			State:       info["state"].(string),
+			PublicKey:   info["publicKey"].(string),
+			SwitchLabel: info["switchLabel"].(string),
+		}
+		response = append(response, peer)
 	}
 	return
 }
 
 //InterfaceController_disconnectPeer(pubkey)
-
