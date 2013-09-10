fad269783a04de2a2b1b3daf68ff0ba762424ae9
inhies <inhies@gmail.com>
2013-09-10 05:37:52 -1000

    Move examples to cjdns package

--- cjdns/example_test.go
+++ cjdns/example_test.go
@@ -1,27 +1,26 @@
-package cjdns_test
+package cjdns
 
 import (
 	"fmt"
-	"github.com/inhies/go-cjdns/cjdns"
 )
 
 func ExampleCheckPubkey() {
-	rawKey, _ := cjdns.ParsePubKey("nss00kzfgckxzju9ynlk5qv9gdljh3wwhtc5dw6d7mk9m2fmgg40.k")
-	valid := cjdns.CheckPubKey(rawKey)
+	rawKey, _ := ParsePubKey("nss00kzfgckxzju9ynlk5qv9gdljh3wwhtc5dw6d7mk9m2fmgg40.k")
+	valid := CheckPubKey(rawKey)
 	fmt.Println(valid)
 	// Output:
 	// true
 }
 
 func ExamplePadIPv6() {
-	addr := cjdns.PadIPv6("fc2d:39c5:be:8dfa:db9d:bf12:7942:b806")
+	addr := PadIPv6("fc2d:39c5:be:8dfa:db9d:bf12:7942:b806")
 	fmt.Println(addr)
 	// Output:
 	// fc2d:39c5:00be:8dfa:db9d:bf12:7942:b806
 }
 
 func ExamplePubKeyToIP() {
-	addr, _ := cjdns.PubKeyToIP("nss00kzfgckxzju9ynlk5qv9gdljh3wwhtc5dw6d7mk9m2fmgg40.k")
+	addr, _ := PubKeyToIP("nss00kzfgckxzju9ynlk5qv9gdljh3wwhtc5dw6d7mk9m2fmgg40.k")
 	fmt.Println(addr)
 	// Output:
 	// fc2d:39c5:00be:8dfa:db9d:bf12:7942:b806
