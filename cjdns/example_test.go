52342187101b7c02ce92f2bbdb910e89108da08a
inhies <inhies@gmail.com>
2013-09-10 16:22:05 -1000

    Add example for pubkey parsing

--- cjdns/example_test.go
+++ cjdns/example_test.go
@@ -1,10 +1,11 @@
 package cjdns
 
 import (
+	"bytes"
 	"fmt"
 )
 
-func ExampleCheckPubkey() {
+func ExampleCheckPubKey() {
 	rawKey, _ := ParsePubKey("r6jzx210usqbgnm3pdtm1z6btd14pvdtkn5j8qnpgqzknpggkuw0.k")
 	valid := CheckPubKey(rawKey)
 	fmt.Println(valid)
@@ -24,4 +25,16 @@ func ExamplePubKeyToIP() {
 	fmt.Println(addr)
 	// Output:
 	// fc68:cb2c:60db:cb96:19ac:34a8:fd34:03fc
+}
+func ExampleParsePubKey() {
+	key, _ := ParsePubKey("r6jzx210usqbgnm3pdtm1z6btd14pvdtkn5j8qnpgqzknpggkuw0.k")
+	valid := bytes.Equal(key, []byte{
+		0xd7, 0xc0, 0xdf, 0x45, 0x00, 0x1a, 0x5b, 0xe5,
+		0xe8, 0x1c, 0x95, 0xe5, 0x19, 0xbe, 0x51, 0x99,
+		0x05, 0x52, 0x37, 0xcb, 0x91, 0x16, 0x88, 0x2c,
+		0xad, 0xce, 0xfe, 0x48, 0xab, 0x73, 0x51, 0x73,
+	})
+	fmt.Println(valid)
+	// Output:
+	// true
 }
