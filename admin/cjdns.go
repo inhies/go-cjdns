a958c1276a11d55e3d3a8470acec89eb4d72aaa4
inhies <inhies@gmail.com>
2013-11-28 22:29:46 -1000

    Move encodpubkey to key pkg

--- admin/cjdns.go
+++ admin/cjdns.go
@@ -314,26 +314,6 @@ func myRand(min, max int, char string) string {
 	return string(buf)
 }
 
-// base32 encodes the public key for use in the decoder
-func EncodePubKey(in []byte) (out []byte) {
-	var wide, bits uint
-	var i2b = []byte("0123456789bcdfghjklmnpqrstuvwxyz")
-
-	for len(in) > 0 {
-		// Add the 8 bits of data from the next `in` byte above the existing bits
-		wide, in, bits = wide|uint(in[0])<<bits, in[1:], bits+8
-		for bits > 5 {
-			// Remove the least significant 5 bits and add their character to out
-			wide, out, bits = wide>>5, append(out, i2b[int(wide&0x1F)]), bits-5
-		}
-	}
-	// If it wasn't a precise multiple of 40 bits, add some padding based on the remaining bits
-	if bits > 0 {
-		out = append(out, i2b[int(wide)])
-	}
-	return out
-}
-
 //// Converts a cjdns public key to an ipv6 address
 //func PubKeyToIP(in []byte) (outString string, err error) {
 //	// Check for the trailing .k
--- key/address.go
+++ key/address.go
@@ -6,6 +6,26 @@ import (
 	"strings"
 )
 
+// base32 encodes the public key for use in the decoder
+func EncodePubKey(in []byte) (out []byte) {
+	var wide, bits uint
+	var i2b = []byte("0123456789bcdfghjklmnpqrstuvwxyz")
+
+	for len(in) > 0 {
+		// Add the 8 bits of data from the next `in` byte above the existing bits
+		wide, in, bits = wide|uint(in[0])<<bits, in[1:], bits+8
+		for bits > 5 {
+			// Remove the least significant 5 bits and add their character to out
+			wide, out, bits = wide>>5, append(out, i2b[int(wide&0x1F)]), bits-5
+		}
+	}
+	// If it wasn't a precise multiple of 40 bits, add some padding based on the remaining bits
+	if bits > 0 {
+		out = append(out, i2b[int(wide)])
+	}
+	return out
+}
+
 // ParsePubKey decodes a public key string to a byte slice
 func ParsePubKey(key string) (raw []byte, err error) {
 	// Check for the trailing .k
