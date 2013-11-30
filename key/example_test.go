df3e5191c2da5b6b38395ab17b34ceaf027929ae
inhies <inhies@gmail.com>
2013-11-29 23:05:30 -1000

    Add examples back

--- key/example_test.go
+++ key/example_test.go
@@ -1,46 +1,40 @@
 package key
 
-import ()
+import (
+	"fmt"
+)
 
-// Need to re-write these for the new key package
-/*
-func ExampleCheckPubKey() {
-	rawKey, _ := ParsePubKey("r6jzx210usqbgnm3pdtm1z6btd14pvdtkn5j8qnpgqzknpggkuw0.k")
-	valid := CheckPubKey(rawKey)
-	fmt.Println(valid)
+func ExampleDecodePrivate() {
+	key, _ := DecodePrivate("751d3db85b848deaf221e0ed2b6cc17f587b29057d74cdd4dc0bd18b7157288e")
+	fmt.Println(key.Valid())
 	// Output:
 	// true
 }
 
-
-func ExamplePadIPv6() {
-	addr := PadIPv6("fc68:cb2c:60db:cb96:19ac:34a8:fd34:3fc")
-	fmt.Println(addr)
+func ExamplePrivate_String() {
+	key, _ := DecodePrivate("751d3db85b848deaf221e0ed2b6cc17f587b29057d74cdd4dc0bd18b7157288e")
+	fmt.Printf("%s\n", key)
 	// Output:
-	// fc68:cb2c:60db:cb96:19ac:34a8:fd34:03fc
+	// 751d3db85b848deaf221e0ed2b6cc17f587b29057d74cdd4dc0bd18b7157288e
 }
 
-func ExamplePubKeyToIP() {
-	addr, _ := PubKeyToIP("r6jzx210usqbgnm3pdtm1z6btd14pvdtkn5j8qnpgqzknpggkuw0.k")
-	fmt.Println(addr)
+func ExamplePrivate_Pubkey() {
+	key, _ := DecodePrivate("751d3db85b848deaf221e0ed2b6cc17f587b29057d74cdd4dc0bd18b7157288e")
+	fmt.Printf("%s\n", key.Pubkey())
 	// Output:
-	// fc68:cb2c:60db:cb96:19ac:34a8:fd34:03fc
+	// r6jzx210usqbgnm3pdtm1z6btd14pvdtkn5j8qnpgqzknpggkuw0.k
 }
 
-func ExampleParsePubKey() {
-	key, err := Decode("r6jzx210usqbgnm3pdtm1z6btd14pvdtkn5j8qnpgqzknpggkuw0.k")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	valid := bytes.Equal(key, []byte{
-		0xd7, 0xc0, 0xdf, 0x45, 0x00, 0x1a, 0x5b, 0xe5,
-		0xe8, 0x1c, 0x95, 0xe5, 0x19, 0xbe, 0x51, 0x99,
-		0x05, 0x52, 0x37, 0xcb, 0x91, 0x16, 0x88, 0x2c,
-		0xad, 0xce, 0xfe, 0x48, 0xab, 0x73, 0x51, 0x73,
-	})
-	fmt.Println(valid)
+func ExampleDecodePublic() {
+	key, _ := DecodePublic("r6jzx210usqbgnm3pdtm1z6btd14pvdtkn5j8qnpgqzknpggkuw0.k")
+	fmt.Println(key.Valid())
 	// Output:
 	// true
 }
-*/
+
+func ExamplePublic_IP() {
+	key, _ := DecodePublic("r6jzx210usqbgnm3pdtm1z6btd14pvdtkn5j8qnpgqzknpggkuw0.k")
+	fmt.Println(key.IP())
+	// Output:
+	// fc68:cb2c:60db:cb96:19ac:34a8:fd34:3fc
+}
